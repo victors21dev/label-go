a8c670b1b071175f6fd3e5b5ef1e7d077dadb477
victors21dev <santos.victors2000@gmail.com>
2026-02-02 14:46:31 -0300

    feat: Adicionado setup react-to-print e next-themes

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
+"use client";
+
 import { Button } from "./_components/_ui/button";
+import { useReactToPrint } from "react-to-print";
+import { useRef } from "react";
 
 export default function Home() {
+  const contentRef = useRef<HTMLDivElement>(null);
+
+  const reactToPrintFn = useReactToPrint({
+    contentRef: contentRef,
+  });
+
   return (
     <div>
-      <Button>Aqui</Button>
+      <Button onClick={() => reactToPrintFn()}>Print</Button>
+      <div className="w-50 h-25 bg-amber-300" ref={contentRef}>
+        <h1>Content to print</h1>
+        <p>This section will be printed.</p>
+      </div>
     </div>
   );
 }
