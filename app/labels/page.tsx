6a7ed0bea2507c523df65f02c9737a4a8b67460b
victors21dev <santos.victors2000@gmail.com>
2026-02-03 14:09:23 -0300

    feat: Configuracao inicial para a etiqueta de impressora

--- app/labels/page.tsx
+++ app/labels/page.tsx
@@ -2,22 +2,42 @@
 
 import { Button } from "../_components/ui/button";
 import { useReactToPrint } from "react-to-print";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import TitleToPage from "../_components/title-page";
 
 const Label = () => {
+  const [larguraLabel, setLarguraLabel] = useState("6,2cm");
+  const [alturaLabel, setAlturaLabel] = useState("6,0cm");
+
   const contentRef = useRef<HTMLDivElement>(null);
+
   const reactToPrintFn = useReactToPrint({
     contentRef: contentRef,
   });
+
+  const convert_mm_to_cm = (mm: number) => {
+    let cm = mm / 10;
+    console.log(cm);
+  };
+
+  const handleLabelGenerator = async (mm: number) => {
+    await convert_mm_to_cm(mm);
+    await reactToPrintFn();
+  };
+
   return (
     <main>
       <div>
         <TitleToPage title="Etiquetas" description="Gere aqui suas etiquetas" />
       </div>
-      <div className="hidden">
-        <Button onClick={() => reactToPrintFn()}>Print</Button>
-        <div className="w-50 h-25 bg-amber-300" ref={contentRef}>
+      <div className="flex gap-2 flex-col">
+        <Button className="w-fit" onClick={() => handleLabelGenerator(62)}>
+          Print
+        </Button>
+        <div
+          className={`w-[6.2cm] h-[4.8cm] border border-muted p-2 rounded-2xl`}
+          ref={contentRef}
+        >
           <h1>Content to print</h1>
           <p>This section will be printed.</p>
         </div>
