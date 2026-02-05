3584ec20a512bd86d1eb5c593164c9db6426ec4f
victors21dev <santos.victors2000@gmail.com>
2026-02-05 11:21:10 -0300

    feat: Adicinado tabela a pagina de historico

---
+++ app/history/_components/table-columns.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { LabelGeneration } from "@/generated/prisma/client";
+import { ColumnDef } from "@tanstack/react-table";
+
+export const historyTableColumns: ColumnDef<LabelGeneration>[] = [
+  {
+    accessorKey: "user",
+    header: "Usuário",
+  },
+  {
+    accessorKey: "sector",
+    header: "Setor",
+  },
+  {
+    accessorKey: "labelModel",
+    header: "Modelo da etiqueta",
+  },
+  {
+    accessorKey: "printer",
+    header: "Impressora",
+  },
+  {
+    accessorKey: "quantity",
+    header: "Quantidade",
+  },
+  {
+    accessorKey: "date",
+    header: "Validade",
+    cell: ({ row }) => {
+      const date = new Date(row.getValue("createdAt"));
+      return new Intl.DateTimeFormat("pt-BR", {
+        timeZone: "America/Sao_Paulo",
+        dateStyle: "short",
+        timeStyle: "short",
+      }).format(date);
+    },
+  },
+  {
+    accessorKey: "createdAt",
+    header: "Data de Criação",
+    cell: ({ row }) => {
+      const date = new Date(row.getValue("createdAt"));
+      return new Intl.DateTimeFormat("pt-BR", {
+        timeZone: "America/Sao_Paulo",
+        dateStyle: "short",
+        timeStyle: "short",
+      }).format(date);
+    },
+  },
+];
--- app/history/page.tsx
+++ app/history/page.tsx
@@ -1,6 +1,10 @@
+import { DataTable } from "../_components/data-table";
 import TitleToPage from "../_components/title-page";
+import { db } from "../_lib/prisma";
+import { historyTableColumns } from "./_components/table-columns";
 
-const History = () => {
+const History = async () => {
+  const dataSector = await db.labelGeneration.findMany();
   return (
     <main className="flex flex-col gap-8">
       <div>
@@ -9,7 +13,9 @@ const History = () => {
           description="O histórico de todas suas etiquetas geradas"
         />
       </div>
-      <div></div>
+      <div>
+        <DataTable columns={historyTableColumns} data={dataSector} />
+      </div>
     </main>
   );
 };
