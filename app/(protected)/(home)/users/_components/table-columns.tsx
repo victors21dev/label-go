25666c76670a0ad24bfbf49ba99b07e8a0988b7f
victors21dev <santos.victors2000@gmail.com>
2026-02-18 10:30:19 -0300

    feat: Adicionado nova interface na tabea de usuário

--- app/(protected)/(home)/users/_components/table-columns.tsx
+++ app/(protected)/(home)/users/_components/table-columns.tsx
@@ -1,12 +1,27 @@
 "use client";
 
+import { DataTableRowActions } from "@/app/_components/data-table-row-actions";
 import { User } from "@/generated/prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
 
 export const userTableColumns: ColumnDef<User>[] = [
   {
-    accessorKey: "nick",
-    header: "Nome do nick",
+    accessorKey: "name",
+    header: "Usuário",
+    cell: ({ row }) => {
+      const name = row.getValue("name") as string;
+      const imageUrl = row.original.imageUrl;
+      return (
+        <div className="flex items-center gap-3">
+          <img
+            className="w-8 h-8 rounded-full object-cover hover:"
+            src={imageUrl || "/default-avatar.png"}
+            alt={`Foto de ${name}`}
+          />
+          <span className="font-medium">{name}</span>
+        </div>
+      );
+    },
   },
   {
     accessorKey: "role",
@@ -23,5 +38,16 @@ export const userTableColumns: ColumnDef<User>[] = [
         timeStyle: "short",
       }).format(date);
     },
+  },
+  {
+    header: "Ações",
+    id: "actions",
+    cell: ({ row }) => (
+      <DataTableRowActions
+        row={row}
+        onEdit={(item) => console.log("Editando", item.name)}
+        onDelete={(item) => console.log("Deletando", item.id)}
+      />
+    ),
   },
 ];
