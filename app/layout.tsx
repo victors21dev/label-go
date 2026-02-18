f251832998363f2ab7304e795907b03cd8718ddb
victors21dev <santos.victors2000@gmail.com>
2026-02-18 16:49:45 -0300

    fix: Correção de hydrated

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
       <html lang="pt-br" suppressHydrationWarning>
         <body
           className={`${inter.className} antialiased bg-muted w-full h-full`}
+          suppressHydrationWarning
         >
           <ThemeProvider
             attribute="class"
