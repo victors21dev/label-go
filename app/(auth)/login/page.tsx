ec1b14d27f89e7b00987f8f6d5a439378e45c9ee
victors21dev <santos.victors2000@gmail.com>
2026-02-19 13:52:37 -0300

    refactor: Modificado login para futuras atualizações

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <div>Removido o login, faça login agora : )</div>
+    </div>
+  );
+}
