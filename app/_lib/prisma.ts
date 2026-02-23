42a6d2fd222fc8143db058b0498451138d5a1794
victors21dev <santos.victors2000@gmail.com>
2026-02-23 16:14:14 -0300

    chore: Atualização de adapter no prisma

--- app/_lib/prisma.ts
+++ app/_lib/prisma.ts
@@ -1,15 +1,13 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const db =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ["query", "error"],
-  });
+// Criamos o pool de conexão do driver 'pg'
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
 
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = db;
-}
+export const db = globalForPrisma.prisma || new PrismaClient({ adapter }); // Passamos o adapter aqui!
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
