7e5db877545533877766cc560618d981ce3e9441
victors21dev <santos.victors2000@gmail.com>
2026-02-23 13:45:15 -0300

    fix: Correcao de versoes do prisma

--- app/_lib/prisma.ts
+++ app/_lib/prisma.ts
@@ -1,10 +1,15 @@
-import "dotenv/config";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
+export const db =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["query", "error"],
+  });
 
-export const db = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = db;
+}
