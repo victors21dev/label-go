e4f86b230e4a7593554283a5d6e4da2e1b0c76b3
victors21dev <santos.victors2000@gmail.com>
2026-02-26 09:07:18 -0300

    refactor: Mudança de layout da etiqueta

--- app/_label-models/refeicao.tsx
+++ app/_label-models/refeicao.tsx
@@ -63,13 +63,13 @@ const LabelRefeicao = ({
       {Array.from({ length: printQtd }).map((_, index) => (
         <div
           key={index}
-          className="flex flex-col w-full h-full gap-4 border p-2 rounded-2xl"
+          className="flex flex-col w-full h-full border p-2 rounded-2xl"
           style={{
             width: `${width}cm`,
             height: `${height}cm`,
           }}
         >
-          <div className="flex flex-col h-full text-sm">
+          <div className="flex flex-col h-full text-[10px] justify-center">
             <div className="justify-center text-center">
               AUTORIZAÇÃO DE <span className="font-bold">REFEIÇÃO</span>
               {/* Linha */}
@@ -82,37 +82,41 @@ const LabelRefeicao = ({
                 }}
               ></div>
             </div>
-            <div className="text-sm flex flex-col mt-1">
-              <div>
-                Setor:
-                <span className="font-bold ml-2">{data.name}</span>
-              </div>
-              <div>
-                Coord.:
-                <span className="font-bold ml-2">{data.coordinatorName}</span>
-              </div>
-            </div>
-            <div className="grid grid-cols-[auto_1fr] gap-2 h-full">
+            <div className="flex w-full h-full">
               <div className="w-fit">
                 <div className="flex items-center h-full">
                   <QRCode
-                    size={80}
+                    size={60}
                     value={`https://valida-qr.vercel.app/?data=${qrCodeLink}`}
                   />
                 </div>
               </div>
-              <div className="relative">
-                <div className="relative h-full">
-                  <div className="flex flex-col h-full justify-center">
-                    <div>
-                      Val.:{" "}
-                      <span className="font-bold ml-2">{dateFormatted}</span>
+              <div className="flex flex-col ml-2">
+                <div className="flex flex-col mt-1">
+                  <div>
+                    Setor:
+                    <span className="font-bold ml-2">{data.name}</span>
+                  </div>
+                  <div>
+                    Coord.:
+                    <span className="font-bold ml-2">
+                      {data.coordinatorName}
+                    </span>
+                  </div>
+                </div>
+                <div className="relative">
+                  <div className="relative h-full">
+                    <div className="flex flex-col h-full justify-center">
+                      <div>
+                        Val.:{" "}
+                        <span className="font-bold ml-2">{dateFormatted}</span>
+                      </div>
+                      <div>
+                        Tipo: {""}
+                        <span className="font-bold ml-2">{mealType}</span>
+                      </div>
+                      <div></div>
                     </div>
-                    <div>
-                      Tipo: {""}
-                      <span className="font-bold ml-2">{mealType}</span>
-                    </div>
-                    <div></div>
                   </div>
                 </div>
               </div>
