6a2608faec7a907ef5893fccbcd87e1a3acd5c28
victors21dev <santos.victors2000@gmail.com>
2026-03-09 13:25:03 -0300

    feat: ASC em labels sectors

--- app/(home)/labels/page.tsx
+++ app/(home)/labels/page.tsx
@@ -3,8 +3,16 @@ import { db } from "@/app/_lib/prisma";
 import LabelClient from "@/app/_components/label-client";
 
 const Label = async () => {
-  const dataOptionLabelSelect = await db.labelModel.findMany();
-  const dataOptionSectorSelect = await db.sector.findMany();
+  const dataOptionLabelSelect = await db.labelModel.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
+  const dataOptionSectorSelect = await db.sector.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
 
   return (
     <main className="flex flex-col gap-8">
