c78770cc55800b47df33bed1db5ac0adbe0768af
victors21dev <santos.victors2000@gmail.com>
2026-03-31 07:11:40 -0300

    chore: Add new host in allowed dev origins

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["192.168.50.180", "localhost"],
+  allowedDevOrigins: ["192.168.50.180", "192.168.50.100", "localhost"],
 };
 
 export default nextConfig;
